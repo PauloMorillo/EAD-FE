e467d90f3b3d697246bf98b04dddc858a28d6699
CARLOS <carlosarguello456@hotmail.com>
2024-07-04 13:22:27 -0500

    ultimos cambios

---
+++ formulario/src/Components/Resultados.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import './Resultados.css'; 
+
+export default function Resultados() {
+   
+    return (
+        <div className="caja">
+            <div className="caja1">
+               
+
+                    </div>
+
+                
+                
+              
+            </div>
+       
+    );
+}
